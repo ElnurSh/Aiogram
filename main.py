99ee6e5d7fd7197355aa4a2d69b0ae3216431f14
ElnurSh <elnur.shiri@gmail.com>
2022-07-08 13:59:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from aiogram.utils import executor
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
-from config import TOKEN
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from pymongo import MongoClient
+from config import *
 from questions import *
 import asyncio
 
+cluster = MongoClient(MongoTOKEN)
+db = cluster["Bot"]
+quiz = db["Quiz"]
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
 
 kb = InlineKeyboardMarkup(
                               inline_keyboard=[
@@ -42,7 +51,13 @@ async def process_start_command(message: types.Message):
                                           is_anonymous=False,
                                           open_period=5,
                                           reply_markup=kb)
-                await asyncio.sleep(6)
+                await asyncio.sleep(10)
+
+
+@dp.poll_answer_handler()
+async def poll_answer(poll_answer: types.PollAnswer):
+    answer = " ".join(map(str,poll_answer.option_ids))
+    quiz.find_one_and_update({"id": 1}, {"$push": {"nam": answer}})
 
 @dp.callback_query_handler(text='next_question')
 async def next_question(call: types.CallbackQuery):
@@ -50,7 +65,9 @@ async def next_question(call: types.CallbackQuery):
 @dp.callback_query_handler(text='cancel')
 async def cancel(call: types.CallbackQuery):
     await call.message.answer('Dayandır')
-    await call.answer()
+
+
+
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
