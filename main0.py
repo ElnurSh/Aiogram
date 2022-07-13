366117987985140262e19a7bf6f79d257b5992a7
ElnurSh <elnur.shiri@gmail.com>
2022-07-13 16:56:53 +0400

    13.16.2022
    + main0

--- main0.py
+++ main0.py
@@ -15,7 +15,7 @@ quiz = db["Quiz"]
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
+print(quiz.distinct('qnum')[0])
 kb = InlineKeyboardMarkup(
                               inline_keyboard=[
                                   [
@@ -34,34 +34,18 @@ kb = InlineKeyboardMarkup(
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    timeout = time.time() + 20
-    for number in range(1, 3):
-        mytext = globals()[f'question{number}']
-        for num, text in enumerate(mytext[1:4]):
-            if text == mytext[-1]:
-                correct_index = num
-                await message.answer_poll(question=mytext[0],
-                                          options=mytext[1:4],
-                                          correct_option_id=correct_index,
-                                          type='quiz',
-                                          is_anonymous=False,
-                                          #open_period=5,
-                                          reply_markup=kb)
-                time.sleep(30)
-'''             
-            if len(quiz.distinct('nam')[-1]) != 0:
-                print('1')
-            time.sleep(3)
-            if len(quiz.distinct('nam')[-1]) != 0:
-                print('2')
-            time.sleep(3)
-            if len(quiz.distinct('nam')[-1]) != 0:
-                print('3')
-            time.sleep(3)
-            if len(quiz.distinct('nam')[-1]) == 0:
-                print('no')
+    for num, text in enumerate(question1[1:4]):
+        if text == question1[-1]:
+            correct_index = num
+            await message.answer_poll(question=question1[0],
+                                        options=question1[1:4],
+                                        correct_option_id=correct_index,
+                                        type='quiz',
+                                        is_anonymous=False,
+                                        #open_period=5,
+                                        reply_markup=kb)
 
-'''
+
 
 
 
@@ -72,7 +56,22 @@ async def poll_answer(poll_answer: types.PollAnswer):
 
 @dp.callback_query_handler(text='next_question')
 async def next_question(call: types.CallbackQuery):
-    await call.message.answer('Növbəti suala keçid')
+    chat_id_ = call.from_user.id
+    print(chat_id_)
+    print(quiz.distinct('qnum')[0])
+    for num, text in enumerate(globals()[f"question{int(quiz.distinct('qnum')[-1])+1}"][1:4]):
+        if text == globals()[f"question{int(quiz.distinct('qnum')[-1])+1}"][-1]:
+            correct_index = num
+            await bot.send_poll(chat_id=chat_id_,
+                                question=globals()[f"question{int(quiz.distinct('qnum')[-1])+1}"][0],
+                                options=globals()[f"question{int(quiz.distinct('qnum')[-1])+1}"][1:4],
+                                correct_option_id=correct_index,
+                                type='quiz',
+                                is_anonymous=False,
+                                # open_period=5,
+                                reply_markup=kb)
+            quiz.find_one_and_update({"id": 1}, {"$push": {"qnum": int(quiz.distinct('qnum')[-1])+1}})
+    #await call.message.answer('Növbəti suala keçid')
 @dp.callback_query_handler(text='cancel')
 async def cancel(call: types.CallbackQuery):
     await call.message.answer('Dayandır')
--- questions.py
+++ questions.py
@@ -1,2 +1,3 @@
 question1 = ['What is my name?', 'elnur', 'vali', 'ali', 'ali']
 question2 = ['Where are you working?', 'SOCAR', 'AZERSUN', 'FUNDA', 'AZERSUN']
+question3 = ['How old i am?', '35', '34', '33', '35']
